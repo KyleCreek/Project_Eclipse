5ae93175aec1457d4a746fadc8663c2fbd8f8534
KyleCreek <kyle.a.creek@gmail.com>
2023-11-25 12:35:55 -0800

    Made New Notes on how to Handle POST / GET Requests

--- app.js
+++ app.js
@@ -12,23 +12,35 @@ app.get('/', (req, res) => {
 app.get('/create_bet', (req, res) =>{
     debugger;
     res.sendFile(path.join(__dirname, 'views/create_bet.html'));
-    console.log('create bet');
+    //const description = document.getElementById('bet-description').value;
+    //console.log(description);
 })
+
+// Create Bet POST
+
 
 // Create User GET
 app.get('/create_user', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/create_user.html'));
 })
 
+// Create User POST
+
 // Browse GET
 app.get('/browse', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/browse.html'));
 })
 
+// Browse POST
+
+
+// Login GET
 app.get('/login', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'views/login.html'));
+    res.send
+    File(path.join(__dirname, 'views/login.html'));
 })
 
+// Login POST
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
